e9bbcaff26f2dbf1e6160adf46712ff8bda23278
tyseer <tyseerhe@gmail.com>
2022-05-08 16:30:27 +0300

    updated

--- js/script.js
+++ js/script.js
@@ -8,15 +8,13 @@ subm.addEventListener("click", () => {
     let emailPattern = /^[a-zA-Z0-9" ".!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
 
     if (email.value.match(emailPattern)) {
-        emailBox.classList.add('valid');
-        emailBox.classList.remove('invalid');
+        emailBox.value=true;
         emailBox.style.color = 'green';
-        emailText.innerHTML = "Your Email Address in Valid";
+        emailText.innerHTML = "Your Email Address is Valid";
 
     }
     else {
-        emailBox.classList.add('invalid');
-        emailBox.classList.remove('valid');
+        emailBox.value=false;
         emailBox.style.color = 'red';
         emailText.innerHTML = "Your Email Address is Invalid";
     }
@@ -33,8 +31,7 @@ subm.addEventListener("click", () => {
     let passpat = /^(?!.*(012|123|234|345|456|567|678|789{3,})).*$/;
 
     if (password.value.match(passPattern)) {
-        passBox.classList.add('valid');
-        passBox.classList.remove('invalid');
+        passBox.value=true;
         passText.style.color = 'green';
         passText.innerHTML = "Your Password is valid";
     }
@@ -43,73 +40,50 @@ subm.addEventListener("click", () => {
     //------------------------------------------------------------
 
     else if (!password.value.match(passnum)) {
-        passBox.classList.add('invalid');
-        passBox.classList.remove('valid');
+        passBox.value=false;
         passText.style.color = 'red';
-
         passText.innerHTML = "Your password doesn't has a numbers";
     }
     //------------------------------------------------------------
 
 
     else if (!password.value.match(passlength)) {
-        passBox.classList.add('invalid');
-        passBox.classList.remove('valid');
+        passBox.value=false;
         passText.style.color = 'red';
-
         passText.innerHTML = "Your password has to be minimum 8 charcters";
     }
     //-------------------------------------------------------------
 
     else if (!password.value.match(specChar)) {
-        passBox.classList.add('invalid');
-        passBox.classList.remove('valid');
+        passBox.value=false;
         passText.style.color = 'red';
-
         passText.innerHTML = "please add special charcters";
     }
     //-------------------------------------------------------------
 
-    
-
-
     if (!password.value.match(passpat)) {
-        passBox.classList.add('invalid');
-        passBox.classList.remove('valid');
+        passBox.value=false;
         passText.style.color = 'red';
         passText.innerHTML = "your password has 3 consecutive numbers.";
-
-
     }
 
     if (!password.value.match(passSmall)) {
-        passBox.classList.add('invalid');
-        passBox.classList.remove('valid');
+        passBox.value=false;
         passText.style.color = 'red';
-
         passText.innerHTML = "Your password doesn't has a lowercase letter"; 
     }
     //-----------------------------------------------------
 
-
     else if (!password.value.match(passBig)) {
-        passBox.classList.add('invalid');
-        passBox.classList.remove('valid');
+        passBox.value=false
         passText.style.color = 'red';
-
         passText.innerHTML = "Your password doesn't has a big letter";
     }
 
     else if (password.value.match(passEng)){
-        passBox.classList.add('invalid');
-        passBox.classList.remove('valid');
+        passBox.value=false;
         passText.style.color = 'red';
-
         passText.innerHTML = "only english please";
     }
-
-    
-    
-     
 });
 
